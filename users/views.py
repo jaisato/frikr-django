583c3c19c8e4be5760aff78f58c1c29e51db874b
Jairo Sarabia <jairosarabia@gmail.com>
2017-10-14 19:47:53 +0200

    Added user login

--- users/views.py
+++ users/views.py
@@ -2,17 +2,35 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout as djlogout
+from django.contrib.auth import logout as dj_logout, authenticate, login as dj_login
 
 
 def login(request):
+	error_msg = []
+	if request.method == 'POST':
+		username = request.POST.get('usr')
+		password = request.POST.get('pwd')
+		user = authenticate(username=username, password=password)
 
-	return None
+		if user is None:
+			error_msg.append('Invalid username or password')
+		else:
+			if user.is_active:
+				dj_login(request, user)
+				return redirect('photos_home')
+			else:
+				error_msg.append('User is not active')
+
+	context = {
+		'errors': error_msg
+	}
+
+	return render(request, 'users/login.html', context)
 
 
 def logout(request):
 
 	if request.user.is_authenticated():
-		djlogout(request)
+		dj_logout(request)
 
 	return redirect('photos_home')
