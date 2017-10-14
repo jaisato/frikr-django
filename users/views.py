c8d963d43257644b68515bd154fe7d89876d0bcf
Jairo Sarabia <jairosarabia@gmail.com>
2017-10-14 20:10:50 +0200

    Using a form class for users login.

--- users/views.py
+++ users/views.py
@@ -3,26 +3,32 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as dj_logout, authenticate, login as dj_login
+from users.forms import LoginForm
 
 
 def login(request):
+	form = LoginForm()
 	error_msg = []
-	if request.method == 'POST':
-		username = request.POST.get('usr')
-		password = request.POST.get('pwd')
-		user = authenticate(username=username, password=password)
 
-		if user is None:
-			error_msg.append('Invalid username or password')
-		else:
-			if user.is_active:
-				dj_login(request, user)
-				return redirect('photos_home')
+	if request.method == 'POST':
+		form = LoginForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('usr')
+			password = form.cleaned_data.get('pwd')
+			user = authenticate(username=username, password=password)
+
+			if user is None:
+				error_msg.append('Invalid username or password')
 			else:
-				error_msg.append('User is not active')
+				if user.is_active:
+					dj_login(request, user)
+					return redirect('photos_home')
+				else:
+					error_msg.append('User is not active')
 
 	context = {
-		'errors': error_msg
+		'errors': error_msg,
+		'login_form': form
 	}
 
 	return render(request, 'users/login.html', context)
