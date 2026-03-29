7c4d0aa8c169ee52aabf39645416ec09889f6141
Jairo Sarabia <jaisato@users.noreply.github.com>
2026-03-29 15:17:30 +0200

    Fix critical security vulnerabilities
    - Replace hardcoded SECRET_KEY with environment variable
    - Set DEBUG from environment variable (default False)
    - Set ALLOWED_HOSTS from environment variable
    - Add authentication (IsAdminUser) to User API endpoints
    - Make password field write-only in UserSerializer

--- frikr/settings.py
+++ frikr/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_*)@a(&k7thzg4aekyet8cewfjmx$z5vz$fnc&z!q)u+l-ptn7'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'change-me-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',') if os.environ.get('DJANGO_ALLOWED_HOSTS') else []
 
 
 # Application definition
--- users/api.py
+++ users/api.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
 from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from users.pagination import UserPageNumberPagination
@@ -12,6 +13,8 @@ class UserListAPI(APIView):
 	"""
 	API view of User list
 	"""
+
+	permission_classes = [IsAdminUser]
 
 	def get(self, request):
 		"""
@@ -46,6 +49,9 @@ class UserDetailAPI(APIView):
 	"""
 	API view of user detail
 	"""
+
+	permission_classes = [IsAdminUser]
+
 	def get(self, request, id):
 		"""
 		Gets user detail
